d53eaba5fc3daf5d909b78aacc8c4b9a9ee2fef0
Jiří Vrba <jiri@vrba.dev>
2021-01-18 19:31:20 +0100

    Another meme, but it's VŠE related

--- src/main/scala/dev/vrba/verification/VerificationListener.scala
+++ src/main/scala/dev/vrba/verification/VerificationListener.scala
@@ -43,7 +43,7 @@ class VerificationListener(private val configuration: Configuration) extends Lis
       case Success =>
         embed.setColor(new Color(0x28a745))
         embed.setTitle("Verifikace proběhla úspěšně.")
-        embed.setImage("https://i.imgur.com/aDwGFLr.jpg")
+        embed.setImage("https://i.imgur.com/4Oa7Cxa.png")
         embed.setDescription("Během chvíle ti bude odemknut plný přístup na server.")
 
         assignVerifiedRoleToUser(event)
